8a80a9f77135c0030c536084620b9a695b49fb1d
gracie17-cloud <graciem7790@gmail.com>
2026-03-11 11:47:27 -0700

    made about me card better / only one

--- my-first-react-app/src/components/AboutMeCard.jsx
+++ my-first-react-app/src/components/AboutMeCard.jsx
@@ -1,8 +1,4 @@
-function AboutMeCard() {
-  const name = "Your Name";
-  const bio = "I like cats.";
-  const funFact = "Fun fact: I can cook.";
-  const emoji = "😊";
+function AboutMeCard({emoji,name,bio,funfact}) {
 
   return (
     <div className="bg-white border border-gray-200 rounded-2xl shadow-md p-6 w-80 text-center hover:shadow-xl hover:-translate-y-1 transition-all duration-200">
@@ -20,7 +16,7 @@ function AboutMeCard() {
       <p className="text-gray-600 mt-2">{bio}</p>
 
       <p className="mt-2">
-        <strong>Fun Fact:</strong> {funFact}
+        <strong>Fun Fact:</strong> {funfact}
       </p>
 
     </div>
