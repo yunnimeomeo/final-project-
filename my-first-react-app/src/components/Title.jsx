617fad20ecdd4867fc88b909b8ce8ab43d303957
luliyaa <lulizemi7@gmail.com>
2026-03-13 12:04:06 -0700

    added title component

---
+++ my-first-react-app/src/components/Title.jsx
@@ -0,0 +1,10 @@
+function Title ({myTitle,mySubtitle}){
+    return(
+        <div className="text-center">
+            <h1 className="italic text-8xl bg-yellow-500 m-4 p-4">{myTitle}</h1>
+            <h3 className="italic font-bold text-2xl bg-green-500 p-4 m-4 shadow-xl w-50 rounded-2xl">{mySubtitle}</h3>
+        </div>
+    )
+};
+
+export default Title;
