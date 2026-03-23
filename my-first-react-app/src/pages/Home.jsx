8ba34462683c0bdecb4e4b660067afd183991d8a
gracie17-cloud <graciem7790@gmail.com>
2026-03-23 11:39:10 -0700

    made cards? i think (test)

--- my-first-react-app/src/pages/Home.jsx
+++ my-first-react-app/src/pages/Home.jsx
@@ -7,8 +7,38 @@ function Home() {
     <>
           <Title myTitle="Gogett3r" mySubtitle="welcome!" />
           <h1></h1>
+       <div>
+        <Title myTitle="About us" mySubtitle="This is us" />
+         <h1>About us Page</h1>
+          <AboutMeCard 
+          name = "Yunni Nguyen"
+  bio = "I like Cat."
+  funfact = "Fun fact: I can cooking"
+  emoji = "😄"
+        />
 
-    <h1>Home Page</h1> 
+         <AboutMeCard 
+          name = "Lulya"
+  bio = "I like traveling"
+  funfact = "i actually like cooking . i do"
+  emoji = "😄"
+        />
+<AboutMeCard 
+          name = "Lelena"
+  bio = "I like swimming"
+  funfact = "I was born in Ethiopia"
+  emoji = "😄"
+        />
+        <AboutMeCard 
+          name = "Gracie"
+  bio = "I like eating"
+  funfact = "I have mild fear of heights!!!"
+  emoji = "😄"
+        />
+
+       </div>)
+
+         ;
 
     </>
     )
